401e8c99382cc1f7c26a66555b2a4efc04eb5468
tran49 <85269921+tran49@users.noreply.github.com>
2024-12-12 13:17:29 -0600

    Update myfuns.py

--- myfuns.py
+++ myfuns.py
@@ -51,14 +51,14 @@ def get_recommended_movies(new_user_ratings):
     rec = myIBCF(cos_similarity_matrix, unew)
 
     ret = pd.DataFrame({'movie_id': rec,
-                        'title': [movies[movies.movie_id == id]['title'].values for id, rating in rec]})
+                        'title': [movies[movies.movie_id == id]['title'].values[0] for id in rec]})
     return ret
 
 
 def get_popular_movies(genre: str):
-    rec = genre_top10[genre].to_list()
+    top10 = genre_top10[genre].to_list()
     ret = pd.DataFrame({'movie_id': rec,
-                        'title': [movies[movies.movie_id == id]['title'].values for id, rating in rec]})
+                        'title': [movies[movies.movie_id == id]['title'].values[0] for id in top10]})
     return ret
     #return movies[movies.movie_id.isin(genre_top10[genre])]
     #return genre_top10[genre].tolist()
@@ -173,6 +173,6 @@ def myIBCF(S, new_user):
         for _, row in remaining_popular_movies.iterrows():
             if len(recommendations) >= 10:
                 break
-            recommendations.append((f"m{row['MovieID']}", f"{row['avg_rating']:.7f}"))
+            recommendations.append((f"m{row['MovieID']}"))
 
     return recommendations
