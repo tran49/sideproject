1905951d00ce7ac63199c76dec8d8e22ec51fc14
tran49 <85269921+tran49@users.noreply.github.com>
2024-12-12 13:40:34 -0600

    Update myfuns.py

--- myfuns.py
+++ myfuns.py
@@ -173,6 +173,7 @@ def myIBCF(S, new_user):
         for _, row in remaining_popular_movies.iterrows():
             if len(recommendations) >= 10:
                 break
+            print('row['MovieID'] : ', row['MovieID'])
             recommendations.append(int(float(row['MovieID'][1:])))
           
     print("recommendation result", recommendations)
