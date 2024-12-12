30552f66f6c7ff46c47ecd7af0c8bc74c10b06c1
tran49 <85269921+tran49@users.noreply.github.com>
2024-12-12 13:09:59 -0600

    Update myfuns.py

--- myfuns.py
+++ myfuns.py
@@ -51,14 +51,14 @@ def get_recommended_movies(new_user_ratings):
     rec = myIBCF(cos_similarity_matrix, unew)
 
     ret = pd.DataFrame({'movie_id': rec,
-                        'title': [movies[movies.movie_id == id]['title'].values[0] for id in rec]})
+                        'title': [movies[movies.movie_id == id]['title'].values[0] for id in rec['MovieID']]})
     return ret
 
 
 def get_popular_movies(genre: str):
     rec = genre_top10[genre].to_list()
     ret = pd.DataFrame({'movie_id': rec,
-                        'title': [movies[movies.movie_id == id]['title'].values[0] for id in rec]})
+                        'title': [movies[movies.movie_id == id]['title'].values[0] for id in rec['MovieID']]})
     return ret
     #return movies[movies.movie_id.isin(genre_top10[genre])]
     #return genre_top10[genre].tolist()
